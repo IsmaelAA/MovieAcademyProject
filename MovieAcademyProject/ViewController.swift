94e16dad9b0a3c7496e15b08e26941efb4ecc75e
Ismael Alba Areces <ismaelalbaareces@Ismaels-MacBook-Pro.local>
2022-02-09 10:48:57 +0100

    Initial XCode Project

---
+++ MovieAcademyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAcademyProject
+//
+//  Created by Ismael Alba Areces on 9/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
